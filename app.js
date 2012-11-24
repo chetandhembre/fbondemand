535f77c5461a786a044d337dc7546900a6af23e9
ChetanD <chetandhembre@gmail.com>
2012-11-25 02:32:12 +0530

    init

---
+++ app.js
@@ -0,0 +1,15 @@
+/**
+ * @author ChetanD
+ */
+
+
+var express=require('express')
+    ,app=express();
+    
+    
+app.get('/',function(req,res){
+	res.sendfile(__dirname+'/index.html');
+});
+
+app.listen(5000);    
+
